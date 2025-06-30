943c0d0c5f7a078ad9450a4615ffc1cc92f334b1
janesam1965 <84298737+janesam1965@users.noreply.github.com>
2025-06-30 11:00:39 +1000

    feat: initialize Express server with dotenv config and add deployment configs

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+
+
 
 const app = express();
 app.use(express.json());
